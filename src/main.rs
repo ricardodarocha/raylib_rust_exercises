7ea911c194cc108b6036137943ace1d55057e12b
Ricardo da Rocha <ricardodarocha@outlook.com>
2025-05-02 20:24:45 -0300

    Create main.rs
    First commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use raylib::prelude::*;
+
+fn main() {
+  let (mut rl, thread) = raylib::init()
+    .size()
+    .title()
+    .vsync()
+    .build();
+
+  while !rl.window_should_close() {
+    let mut d = rl.begin_drawing(&thread);
+    d.clear_background(Color::BLACK);
+    d.draw_text("Hello, World", 12, 12, 34, Color::WHITE);
+  }
+}
